d8e35d6c89cd2a9c62c8a948d150bab131c78a68
Barclay Josué <66049385+josuebarclay@users.noreply.github.com>
2023-02-03 13:40:35 -0400

    Add files via upload

---
+++ GerenciadorApplication.java
@@ -0,0 +1,17 @@
+package br.com.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Equipe Autocom Informatica]
+ */
+
+@SpringBootApplication
+public class GerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorApplication.class, args);
+	}
+
+}
